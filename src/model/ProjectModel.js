73e33c632fa73d240089f8b13e54aaef93d47b89
ArnabTo <arnabs929@gmail.com>
2023-12-29 20:42:05 +0600

    updated projectshema

--- src/model/ProjectModel.js
+++ src/model/ProjectModel.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose')
 
 const projectSchema = new mongoose.Schema({
-
+    title: {
+        type: String,
+        required: true,
+      },
+      image: {
+        type: String,
+        required: true,
+      },
+      github: {
+        type: String,
+        required: true,
+      },
+      live: {
+        type: String,
+        required: true,
+      },
+      technologies: {
+        type: [String], 
+        required: true,
+      },
 })
 
 const Projects = mongoose.model('Projects', projectSchema, "projectCollection")
